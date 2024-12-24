01be65f8b676db66d4bbf826694d6a4cb32e019a
Fercanber <116292124+Fercanber@users.noreply.github.com>
2024-12-24 15:26:56 +0100

    pide un servicio que ya esta implementado en vaadin

--- web-it-uca/src/main/java/es/uca/iw/webituca/Service/EmailService.java
+++ web-it-uca/src/main/java/es/uca/iw/webituca/Service/EmailService.java
@@ -82,4 +82,15 @@ public class EmailService {
 
         return true;
     }
+
+    public void sendEmail(String to, String subject, String text) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(text, true);
+
+        mailSender.send(message);
+    }
 }
---
+++ web-it-uca/src/main/java/es/uca/iw/webituca/Service/JavaMailSender.java
@@ -0,0 +1,8 @@
+package es.uca.iw.webituca.Service;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
