d56dd09bb32f1f6c86d8f29689ebb8117150e215
Fercanber <116292124+Fercanber@users.noreply.github.com>
2024-12-28 00:22:23 +0100

    añadido mail config y sender nuevo
    sigue sin generar codigos ni mandar correos pero no da error

--- web-it-uca/src/main/java/es/uca/iw/webituca/Service/JavaMailSender.java
+++
@@ -1,8 +0,0 @@
-package es.uca.iw.webituca.Service;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
-
-import java.util.Properties;
